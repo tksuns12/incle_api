5ed8d8a9c63aface67beb43a04fcaf55d24e9b7d
강현길 <tksuns@gmail.com>
2022-02-20 21:33:01 +0900

    오늘까지 구현된 postman에 올라와 있는 api 통신 구현

--- lib/incle_api.dart
+++ lib/incle_api.dart
@@ -3,6 +3,7 @@ library incle_api;
 import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
 class IncleAPI {
   static String _userToken = '';
@@ -29,7 +30,8 @@ class IncleAPI {
     String? accountNumber,
     String? accountOwner,
     String? storeCategories,
-    String? storeAddress,
+    String? storeAddress1,
+    String? storeAddress2,
     String? storePhone,
     String? openTime,
     String? closeTime,
@@ -38,42 +40,59 @@ class IncleAPI {
     List<File>? storePictures,
     String? storeName,
     bool? isRestHolidy,
+    double? latitude,
+    double? longitude,
+    String? postCode,
   }) async {
     try {
-      final response = await _dio.post(
-        '/partners',
-        data: {
-          'isRestHolidy': isRestHolidy,
-          'id': id,
-          'password': password,
-          'name': name,
-          'phone_number': phoneNumber,
-          'email': email,
-          'owner_name': ownerName,
-          'business_number': businessNumber,
-          'registration': registration,
-          'registration2': registration2,
-          'bank': bank,
-          'account_number': accountNumber,
-          'account_owner': accountOwner,
-          'store_categories': storeCategories,
-          'store_address': storeAddress,
-          'store_phone': storePhone,
-          'open_time': openTime,
-          'close_time': closeTime,
-          'dayoffs': dayoffs,
-          'store_description': storeDescription,
-          'store_pictures': storePictures,
-          'store_name': storeName,
-        },
-      );
+      final formData = FormData.fromMap({
+        'isRestHoliday': isRestHolidy,
+        'id': id,
+        'password': password,
+        'name': name,
+        'ownerPhone': phoneNumber,
+        'email': email,
+        'ownerName': ownerName,
+        'businessNumber': businessNumber,
+        'accountBank': bank,
+        'accountNumber': accountNumber,
+        'accountName': accountOwner,
+        'targetGender': storeCategories,
+        'location': storeAddress1,
+        'locationDetail': storeAddress2,
+        'postNumber': postCode,
+        'phone': storePhone,
+        'startDate': openTime,
+        'endDate': closeTime,
+        'closedDays': dayoffs,
+        'store_description': storeDescription,
+        'store_name': storeName,
+        'latitude': latitude,
+        'longitude': longitude,
+      });
+
+      formData.files.addAll(storePictures!.map((e) => MapEntry(
+          'storeImages',
+          MultipartFile.fromFileSync(e.path,
+              filename: e.path.split('/').last))));
+      formData.files.add(MapEntry(
+          'businessReport',
+          MultipartFile.fromFileSync(registration!.path,
+              filename: registration.path.split('/').last)));
+      formData.files.add(MapEntry(
+          'businessRegistration',
+          MultipartFile.fromFileSync(registration2!.path,
+              filename: registration2.path.split('/').last)));
+
+      final response =
+          await _dio.post('/partners', data: formData, options: Options());
       if (response.statusCode == 201) {
         return response.data;
       } else {
         throw Exception(response.statusMessage);
       }
     } catch (e) {
-      throw Exception(e.toString());
+      rethrow;
     }
   }
 
@@ -91,7 +110,8 @@ class IncleAPI {
     String? accountNumber,
     String? accountOwner,
     String? storeCategories,
-    String? storeAddress,
+    String? storeAddress1,
+    String? storeAddress2,
     String? storePhone,
     String? openTime,
     String? closeTime,
@@ -100,37 +120,55 @@ class IncleAPI {
     List<File>? storePictures,
     String? storeName,
     bool? isRestHolidy,
+    double? latitude,
+    double? longitude,
+    String? postCode,
   }) async {
     try {
+      final formData = FormData.fromMap({
+        'isRestHoliday': isRestHolidy,
+        'id': id,
+        'password': password,
+        'name': name,
+        'ownerPhone': phoneNumber,
+        'email': email,
+        'ownerName': ownerName,
+        'businessNumber': businessNumber,
+        'accountBank': bank,
+        'accountNumber': accountNumber,
+        'accountName': accountOwner,
+        'targetGender': storeCategories,
+        'location': storeAddress1,
+        'locationDetail': storeAddress2,
+        'postNumber': postCode,
+        'phone': storePhone,
+        'startDate': openTime,
+        'endDate': closeTime,
+        'closedDays': dayoffs,
+        'store_description': storeDescription,
+        'store_name': storeName,
+        'latitude': latitude,
+        'longitude': longitude,
+      });
+
+      formData.files.addAll(storePictures!.map((e) => MapEntry(
+          'storeImages',
+          MultipartFile.fromFileSync(e.path,
+              filename: e.path.split('/').last))));
+      formData.files.add(MapEntry(
+          'businessReport',
+          MultipartFile.fromFileSync(registration!.path,
+              filename: registration.path.split('/').last)));
+      formData.files.add(MapEntry(
+          'businessRegistration',
+          MultipartFile.fromFileSync(registration2!.path,
+              filename: registration2.path.split('/').last)));
       final response = await _dio.put(
         '/partners',
         options: Options(headers: {
-          'Authorization': 'Bearer $_userToken',
+          'Token': 'Bearer $_userToken',
         }),
-        data: {
-          'isRestHolidy': isRestHolidy,
-          'id': id,
-          'password': password,
-          'name': name,
-          'phone_number': phoneNumber,
-          'email': email,
-          'owner_name': ownerName,
-          'business_number': businessNumber,
-          'registration': registration,
-          'registration2': registration2,
-          'bank': bank,
-          'account_number': accountNumber,
-          'account_owner': accountOwner,
-          'store_categories': storeCategories,
-          'store_address': storeAddress,
-          'store_phone': storePhone,
-          'open_time': openTime,
-          'close_time': closeTime,
-          'dayoffs': dayoffs,
-          'store_description': storeDescription,
-          'store_pictures': storePictures,
-          'store_name': storeName,
-        },
+        data: formData,
       );
       if (response.statusCode == 201) {
         return response.data;
@@ -138,11 +176,11 @@ class IncleAPI {
         throw Exception(response.statusMessage);
       }
     } catch (e) {
-      throw Exception(e.toString());
+      rethrow;
     }
   }
 
-  Future<Map> signin(String id, String password) async {
+  Future<Map> login(String id, String password) async {
     try {
       final response = await _dio.post(
         '/partners/login',
@@ -157,7 +195,7 @@ class IncleAPI {
         throw Exception(response.statusMessage);
       }
     } catch (e) {
-      throw Exception(e.toString());
+      rethrow;
     }
   }
 
@@ -167,7 +205,7 @@ class IncleAPI {
         '/partners',
         options: Options(
           headers: {
-            'Authorization': 'Bearer $_userToken',
+            'Token': 'Bearer $_userToken',
           },
         ),
       );
@@ -177,7 +215,86 @@ class IncleAPI {
         throw Exception(response.statusMessage);
       }
     } catch (e) {
-      throw Exception(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Map> deleteAccount() async {
+    try {
+      final response = await _dio.delete(
+        '/partners',
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map> findPasswordAllow(String email, String phone, String name) async {
+    try {
+      final response = await _dio.post(
+        '/partners/find/password/allow',
+        data: {
+          'email': email,
+          'phone': phone,
+          'name': name,
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map> finidPassword(String code, String password) async {
+    try {
+      final response = await _dio.post(
+        '/partners/find/password',
+        data: {
+          'code': code,
+          'password': password,
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map> findId(String email, String name, String phone) async {
+    try {
+      final response = await _dio.post(
+        '/partners/find/id',
+        data: {
+          'email': email,
+          'name': name,
+          'phone': phone,
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 
@@ -185,7 +302,7 @@ class IncleAPI {
   Future<Map> duplicateCheck(String name, String property) async {
     try {
       final response = await _dio.get(
-        '/partners/duplicateCheck',
+        '/partners/duplicate',
         queryParameters: {
           'name': name,
           'property': property,
@@ -216,6 +333,218 @@ class IncleAPI {
       }
     } catch (e) {
       throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> checkVerifyNum(String phoneNumber, String code) async {
+    try {
+      final response = await _dio.post(
+        '/partners/verification',
+        queryParameters: {
+          'phone': phoneNumber,
+          'code': code,
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> createCoupon(
+      String name, int price, int condition, DateTime limitDate) async {
+    try {
+      final response = await _dio.post(
+        '/partners/coupon',
+        data: {
+          'name': name,
+          'price': price,
+          'condition': condition,
+          'limit_date':
+              '${limitDate.year.toString().substring(2, 4)}.${limitDate.month.toString().padLeft(2, '0')}.${limitDate.day.toString().padLeft(2, '0')}',
+        },
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 201) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> getCouponList() async {
+    try {
+      final response = await _dio.get(
+        '/partners/coupon/list',
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> deleteCoupon(String couponID) async {
+    try {
+      final response = await _dio.delete(
+        '/partners/coupon',
+        queryParameters: {
+          'partners_coupon_uid': couponID,
+        },
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> getDeliver() async {
+    try {
+      final response = await _dio.get(
+        '/partners/deliver',
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<Map> updateDeliver(
+      List<Map<String, int>> deliveryConditions, int freeCondition) async {
+    try {
+      final response = await _dio.post('/partners/deliver',
+          data: {
+            'delivery_conditions': deliveryConditions,
+            'free_condition': freeCondition,
+          },
+          options: Options(
+            headers: {
+              'Token': 'Bearer $_userToken',
+            },
+          ));
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map> unpausePartners() async {
+    try {
+      final response = await _dio.post(
+        '/partners/unpause',
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Map> pausePartners(TimeOfDay startTime, TimeOfDay endTime) async {
+    try {
+      final response = await _dio.post(
+        '/partners/pause',
+        data: {
+          'pause_start_date':
+              '${startTime.hour.toString().padLeft(2, '0')}:${startTime.minute.toString().padLeft(2, '0')}',
+          'pause_end_date':
+              '${endTime.hour.toString().padLeft(2, '0')}:${endTime.minute.toString().padLeft(2, '0')}',
+        },
+        options: Options(
+          headers: {
+            'Token': 'Bearer $_userToken',
+          },
+        ),
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<bool> isServerHealthy() async {
+    try {
+      final response = await _dio.get('/health');
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> holidayCheck(String year, String month, String day) async {
+    try {
+      final response = await _dio.get(
+        '/partners/holiday',
+        queryParameters: {
+          'year': year,
+          'month': month,
+          'date': day,
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data['is_holiday'];
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } catch (e) {
+      rethrow;
     }
   }
 }
