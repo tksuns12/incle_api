6735756303653fef2d48fb4b22f114e26acdf34e
강현길 <tksuns@gmail.com>
2022-03-31 20:46:56 +0900

    토큰 갱신 알고리즘 수정

--- lib/dio_client.dart
+++ lib/dio_client.dart
@@ -108,7 +108,37 @@ Dio getPartnersDioClient(
         final refreshDio = Dio()
           ..options = BaseOptions(
             baseUrl: 'http://backend.wim.kro.kr:5000/api/v1',
-          );
+          )
+          ..interceptors.add(InterceptorsWrapper(
+            onError: (e, handler) async {
+              final id = await storage.read(key: 'id');
+              final password = await storage.read(key: 'password');
+              final loginDio = Dio()
+                ..options = BaseOptions(
+                    baseUrl: 'http://backend.wim.kro.kr:5000/api/v1');
+              final response = await loginDio.post(
+                '/login-partners',
+                data: {'id': id, 'password': password},
+              );
+              if (response.statusCode == 200) {
+                storage.write(
+                    key: 'accessToken', value: response.data['accessToken']);
+                storage.write(
+                    key: 'refreshToken', value: response.data['refreshToken']);
+                error.requestOptions.headers['Authorization'] =
+                    response.data['accessToken'];
+                final retryReq =
+                    await loginDio.request(error.requestOptions.path,
+                        options: Options(
+                          method: error.requestOptions.method,
+                          headers: error.requestOptions.headers,
+                        ));
+                return handler.resolve(retryReq);
+              } else {
+                throw Exception(response.statusMessage);
+              }
+            },
+          ));
         refreshDio.options.headers['Authorization'] = 'Bearer $refreshToken';
         final response = await refreshDio.post(
           '/refresh',
@@ -126,34 +156,6 @@ Dio getPartnersDioClient(
                 headers: error.requestOptions.headers,
               ));
           return handler.resolve(retryReq);
-        } else if (response.statusCode == 401) {
-          final id = await storage.read(key: 'id');
-          final password = await storage.read(key: 'password');
-          final loginDio = Dio()
-            ..options =
-                BaseOptions(baseUrl: 'http://backend.wim.kro.kr:5000/api/v1');
-          final response = await loginDio.post(
-            '/login-partners',
-            data: {'id': id, 'password': password},
-          );
-          if (response.statusCode == 200) {
-            storage.write(
-                key: 'accessToken', value: response.data['accessToken']);
-            storage.write(
-                key: 'refreshToken', value: response.data['refreshToken']);
-            error.requestOptions.headers['Authorization'] =
-                response.data['accessToken'];
-            final retryReq = await loginDio.request(error.requestOptions.path,
-                options: Options(
-                  method: error.requestOptions.method,
-                  headers: error.requestOptions.headers,
-                ));
-            return handler.resolve(retryReq);
-          } else {
-            throw Exception(response.statusMessage);
-          }
-        } else {
-          throw Exception(response.statusMessage);
         }
       } else {
         return handler.next(error);
