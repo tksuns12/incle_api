346ff05ec662dc2746ba550fb7d6b74309d15259
nammunsik <70309161+nammunsik@users.noreply.github.com>
2022-06-28 20:37:41 +0900

    주문 상품에 쿠폰을 nullable하게 변경

--- lib/data.dart
+++ lib/data.dart
@@ -1,12 +1,12 @@
 part of 'incle_api.dart';
 
 class OrderProducts {
-  final String couponUid;
+  final String? couponUid;
   final String productOptionGroupUid;
   final int quantity;
 
   OrderProducts(
-      {required this.couponUid,
+      {this.couponUid,
       required this.productOptionGroupUid,
       required this.quantity});
 }
